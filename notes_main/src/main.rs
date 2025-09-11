3a0f9e2bf624fb6aab479c841cffd4b80b5df213
adecess <axel.saliniere@protonmail.com>
2025-09-11 23:14:17 +0200

    set up simple web server

--- notes_main/src/main.rs
+++ notes_main/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Json, Router, http::StatusCode, routing::get};
+use serde::Serialize;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello_json));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_json() -> (StatusCode, Json<Response>) {
+    let response = Response {
+        message: "Hello, world!",
+    };
+
+    (StatusCode::OK, Json(response))
+}
+
+#[derive(Serialize)]
+struct Response {
+    message: &'static str,
 }
