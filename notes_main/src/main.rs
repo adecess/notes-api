8d45da8b17137df0a5c134cf881428bd35917bbe
adecess <axel.saliniere@protonmail.com>
2025-09-12 15:31:16 +0200

    error handling

--- notes_main/src/main.rs
+++ notes_main/src/main.rs
@@ -1,23 +1,55 @@
-use axum::{Json, Router, http::StatusCode, routing::get};
+use anyhow::Context;
+use axum::{Json, Router, http::StatusCode, response::IntoResponse, routing::get};
 use serde::Serialize;
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(hello_json));
+async fn main() -> anyhow::Result<()> {
+    let app = Router::new()
+        .route("/", get(hello_json))
+        .layer(tower_http::catch_panic::CatchPanicLayer::new());
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .context("failed to bind TCP listener")?;
+    axum::serve(listener, app)
+        .await
+        .context("axum::serve failed")?;
+
+    Ok(())
 }
 
-async fn hello_json() -> (StatusCode, Json<Response>) {
+async fn hello_json() -> Result<(StatusCode, Json<Response>), AppError> {
     let response = Response {
-        message: "Hello, world!",
+        message: generate_message().context("failed to generate message")?,
     };
 
-    (StatusCode::OK, Json(response))
+    Ok((StatusCode::OK, Json(response)))
+}
+
+/// Generates the hello world message.
+fn generate_message() -> anyhow::Result<&'static str> {
+    if rand::random() {
+        anyhow::bail!("no message for you");
+    }
+    Ok("Hello, world!")
 }
 
 #[derive(Serialize)]
 struct Response {
     message: &'static str,
+}
+
+struct AppError(anyhow::Error);
+
+// This allows ? to automatically convert anyhow::Error to AppError
+impl From<anyhow::Error> for AppError {
+    fn from(value: anyhow::Error) -> Self {
+        Self(value)
+    }
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, self.0.to_string()).into_response()
+    }
 }
