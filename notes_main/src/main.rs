3d9e7682c79aee79fefe803bc9bb4b5821df4ba7
adecess <axel.saliniere@protonmail.com>
2025-09-21 13:03:54 +0200

    add validator crate and lib.rs

--- notes_main/src/main.rs
+++ notes_main/src/main.rs
@@ -30,7 +30,6 @@ async fn main() {
         .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-
     println!("Server running on http://localhost:3000");
 
     axum::serve(listener, app).await.unwrap();
